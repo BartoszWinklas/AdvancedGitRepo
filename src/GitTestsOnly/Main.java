90de851ba70db041ed9faf963616a455e371306e
Lix <winklas.bartosz@gmail.com>
2018-08-18 23:19:27 +0200

    2nd commit to main.java

--- src/GitTestsOnly/Main.java
+++ src/GitTestsOnly/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        // hello this is another repo for GIT exercise
-
+    // hello this is another repo for GIT exercise
+    // 1st init commit
         System.out.println("HELLO THERE GIT DUDE");
+    // 2ND COMMIT
+        int x;
+        int y;
+        String name;
     }
 }
